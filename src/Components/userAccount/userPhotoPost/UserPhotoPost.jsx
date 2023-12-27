724ca0d12da0f9e07e9de78b21d47204b5e64085
Lucas Cezar <lucas.oheisen@gmail.com>
2023-12-27 18:17:39 -0300

    fix component error. feat: photo preview

--- src/Components/userAccount/userPhotoPost/UserPhotoPost.jsx
+++ src/Components/userAccount/userPhotoPost/UserPhotoPost.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from './userPhotoPost.module.css'
 import InputForm from './../../forms/inputForm/InputForm'
+import BtnForm from '../../forms/btnForm/BtnForm'
 import useForm from '../../../useHooks/useForm'
 import useFetch from '../../../useHooks/useFetch'
 import { PHOTO_POST } from '../../../apiSrc/api'
+import Loading from '../../loading/Loading'
+import { ToastContainer, toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
 const UserPhotoPost = () => {
           const nome = useForm()
           const peso = useForm('number')
@@ -11,9 +15,26 @@ const UserPhotoPost = () => {
           const [img, setImg] = React.useState({})
 
           const { loading, data, error, request } = useFetch()
+          const notify = toast.error(error)
+
+          const navigate = useNavigate()
+
+          React.useEffect(() => {
+
+                    if (data) {
+                              navigate('/')
+                    }
+
+          }, [data, navigate])
 
           async function handleSubmit(event) {
                     event.preventDefault()
+                    if (error) {
+
+
+                              return notify()
+
+                    }
                     const formData = new FormData()
                     formData.append('img', img.raw)
                     formData.append('nome', nome.value)
@@ -23,26 +44,55 @@ const UserPhotoPost = () => {
                     const token = window.localStorage.getItem('token')
                     const { url, options } = PHOTO_POST(token, formData)
                     const { response } = await request(url, options)
-                    console.log(response)
+
+
           }
 
           function handleImgChange({ target }) {
                     setImg({
+                              preview: URL.createObjectURL(target.files[0]),
                               raw: target.files[0],
                     })
+
           }
-          return (
+
+
+          return (<>
+
                     <section className={`${styles.photoPost} animeLeft`} >
 
-                              <form onSubmit={handleSubmit}>
 
-                                        <InputForm label='Nome' type='text' name='nome' {...nome} />
-                                        <InputForm label='Peso' type='text' name='peso' {...peso} />
-                                        <InputForm label='Idade' type='text' name='idade' {...idade} />
-                                        <input type='file' name='img' id='img' onChange={handleImgChange} />
-                                        <button>Enviar</button>
-                              </form>
+                              {loading ? <Loading /> :
+                                        <form onSubmit={handleSubmit}>
+
+                                                  <InputForm texto='Nome' type='text' name='nome' {...nome} />
+                                                  <InputForm texto='Peso' type='number' name='peso' {...peso} />
+                                                  <InputForm texto='Idade' type='number' name='idade' {...idade} />
+
+                                                  <input type='file' name='img' id='img' onChange={handleImgChange} className={styles.file} />
+                                                  <BtnForm>ENVIAR</BtnForm>
+                                        </form>
+                              }
+                              <div>
+                                        {img.preview &&
+                                                  <div className={styles.preview} style={{ backgroundImage: `url('${img.preview}')` }}>
+
+                                                  </div>}
+                              </div>
                     </section>
+                    {error && <ToastContainer
+                              position="top-center"
+                              autoClose={2500}
+                              limit={1}
+                              hideProgressBar={false}
+                              newestOnTop={true}
+                              closeOnClick
+                              rtl={false}
+                              pauseOnFocusLoss
+                              draggable
+                              pauseOnHover
+                              theme='dark' />}
+          </>
           )
 }
 
